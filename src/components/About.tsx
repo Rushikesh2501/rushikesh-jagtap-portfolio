d08733f56470d04a94efb708174955004b23297f
Rushikesh <rushikesh.jagtap@uopeople.edu>
2026-03-11 12:57:32 +0530

    Initial commit of portfolio as Rushikesh Jagtap

---
+++ src/components/About.tsx
@@ -0,0 +1,16 @@
+import "./styles/About.css";
+
+const About = () => {
+  return (
+    <div className="about-section" id="about">
+      <div className="about-me">
+        <h3 className="title">About Me</h3>
+        <p className="para">
+          I build scalable and maintainable web applications using React, TypeScript, and C#. At VK Innovations, I work on platforms in the education domain, contributing to both frontend and backend development while also supporting the team as a Scrum Master. I also have knowledge of Python and continuously work on improving my technical skills.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default About;
